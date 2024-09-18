b75a60b13e35f4585339d328cb939e4ef98eb9e6
TungLam Phan Dam <126652156+tunglampd@users.noreply.github.com>
2024-09-18 20:08:34 +0700

    init

---
+++ Google repo clone/Google repo clone/Google_repo_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Google_repo_cloneApp.swift
+//  Google repo clone
+//
+//  Created by phan dam tung lam on 18/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Google_repo_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Google repo clone/Google repo cloneUITests/Google_repo_cloneUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Google_repo_cloneUITestsLaunchTests.swift
+//  Google repo cloneUITests
+//
+//  Created by phan dam tung lam on 18/9/24.
+//
+
+import XCTest
+
+final class Google_repo_cloneUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
